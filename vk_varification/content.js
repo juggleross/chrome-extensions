a419932496e86e1be6e0e73b772d972d6eefacf7
Vlad Hilko <vladislav.hilko@monostacklabs.com>
2018-03-25 18:03:55 +0300

    Fixed bage duplication

--- vk_varification/content.js
+++ vk_varification/content.js
@@ -1,14 +1,28 @@
-var page_name = document.getElementsByClassName("page_name")[0];
+if(varificationBageDoesNotExist()) addVerificationBage();
 
-verification = document.createElement('a');
-verification.setAttribute('href', '/verify');
-verification.className = 'page_verified';
 
-verification.onmouseover = function() {
-  pageVerifiedTip(this, {type: 1, oid: 1});
-};
+function addVerificationBage() {
+  var page_name = document.getElementsByClassName("page_name")[0];
 
-page_name.appendChild(verification);
+  verification = document.createElement('a');
+  verification.setAttribute('href', '/verify');
+  verification.className = 'page_verified';
+
+  verification.onmouseover = function() {
+    pageVerifiedTip(this, {type: 1, oid: 1});
+  };
+
+  page_name.appendChild(verification);
+}
+
+function varificationBageDoesNotExist() {
+  if(document.getElementsByClassName('page_verified').length < 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 
 

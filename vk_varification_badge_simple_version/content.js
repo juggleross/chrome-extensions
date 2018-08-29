1c8f9d01905aa81181b5650dcafa02cb8f19fcb8
Vladislav Hilko <vladislav.spawn@gmail.com>
2018-08-29 22:14:11 +0300

    Add observers for tracking page changing

--- vk_varification_badge_simple_version/content.js
+++ vk_varification_badge_simple_version/content.js
@@ -1,5 +1,3 @@
-if(varificationBageDoesNotExist()) addVerificationBage();
-
 function addVerificationBage() {
   let page_name = document.getElementsByClassName("page_name")[0];
 
@@ -7,9 +5,26 @@ function addVerificationBage() {
   verification.setAttribute('href', '/verify');
   verification.className = 'page_verified';
 
-  page_name.appendChild(verification);
+  if(page_name !== undefined) page_name.appendChild(verification);
 }
 
 function varificationBageDoesNotExist() {
   return document.getElementsByClassName('page_verified').length < 1 ? true : false
 }
+
+
+// выбираем целевой элемент
+var target = document.querySelector('#utils');
+
+// создаём экземпляр MutationObserver
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if(varificationBageDoesNotExist()) addVerificationBage();
+  });
+});
+
+// конфигурация нашего observer:
+var config = { attributes: true, childList: true, characterData: true };
+
+// передаём в качестве аргументов целевой элемент и его конфигурацию
+observer.observe(target, config);
